a31561f70480af2ca8101dcfe31d59fb8abfc108
alex_klymenko <klymenko.sasha21@gmail.com>
2020-03-02 18:56:03 +0200

    feat: implement count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let result = 0;
+
+  matrix.forEach(innerArr => {
+    innerArr.forEach(item => {
+      result += Number(item === '^^');
+    });
+  });
+
+  return result;
 };
