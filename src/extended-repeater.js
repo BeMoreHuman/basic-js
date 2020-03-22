e1239365327b5a59eb319bc6c36ecc19f7fc653e
alex_klymenko <klymenko.sasha21@gmail.com>
2020-03-22 14:26:25 +0200

    feat: repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,27 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (!options) {
+        return str;
+    }
+    const repeatTimes = options.repeatTimes;
+    const separator = options.separator || '+';
+    const addition = String(options.addition);
+    const additionRepeatTimes = options.additionRepeatTimes || 0;
+    const additionSeparator = options.additionSeparator || '|';
+    const base = (options.addition === undefined) ? String(str) : (String(str) + addition);
+    let result = base;
+
+    for (let i = 0; i < repeatTimes; i++) {
+
+        for (let k = 1; k < additionRepeatTimes; k++) {
+            result += `${additionSeparator}${addition}`;
+        }
+
+        if (i !== repeatTimes - 1) {
+            result += `${separator}${base}`;   
+        }
+
+    }
+
+    return result;
 };
   
