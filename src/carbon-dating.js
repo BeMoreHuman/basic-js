40b8962a70380685a40f21f338e08e61d6df4626
alex_klymenko <klymenko.sasha21@gmail.com>
2020-03-22 12:50:53 +0200

    feat: carbon-dating & dream-team tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || !sampleActivity ||
+    (parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > 15) ||
+    isNaN(parseFloat(sampleActivity))) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const ln = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity));
+  const t = Math.ceil(ln / k);
+
+  return t;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || members.length < 0) {
+    return false;
+  }
+  let secretName = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      secretName += String(members[i].trim().slice(0, 1)).toUpperCase();
+    }
+  }
+  return secretName.split('').sort((a, b) => a.localeCompare(b)).join('');
 };
