11ec78798bd84c36b852d074f2af762dcc51a10e
alex_klymenko <klymenko.sasha21@gmail.com>
2021-03-03 17:08:46 +0200

    feat: getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,32 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  if (!isValidDate(date)) {
+    throw "You provide incorrect argument type!";
+  }
+  const month = date.getMonth() + 1;
+
+  if (3 <= month && month <= 5) {
+    return "spring";
+  }
+
+  if (6 <= month && month <= 8) {
+    return "summer";
+  }
+
+  if (9 <= month && month <= 11) {
+    return "fall";
+  }
+
+  // Months 12, 01, 02
+  return "winter";
 };
+
+function isValidDate(date) {
+  return (
+    date &&
+    Object.prototype.toString.call(date) === "[object Date]" &&
+    !isNaN(date)
+  );
+}
