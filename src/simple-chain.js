fb4f4f45e0c37052f04bd46dc6723c060ad76e06
alex_klymenko <klymenko.sasha21@gmail.com>
2021-03-05 00:18:36 +0200

    feat: simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,54 @@
 const chainMaker = {
+  count: 0,
+  chain: "",
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.count;
   },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  addLink(...value) {
+    this.count++;
+    if (value.length) {
+      this.chain += this.count > 1 ? `~~( ${value[0]} )` : `( ${value[0]} )`;
+    } else {
+      this.chain += `~~( )`;
+    }
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!isValidPosition(position, this.chain)) {
+      this.resetChain();
+      throw "Notexisting link position";
+    }
+    this.count--;
+    const chainArr = this.chain.split("~~");
+    chainArr.splice(position - 1, 1);
+    this.chain = chainArr.join("~~");
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain = this.chain.split("~~").reverse().join("~~");
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    const _chain = this.chain;
+    this.resetChain();
+    return _chain;
+  },
+  resetChain() {
+    this.count = 0;
+    this.chain = "";
+  },
 };
+function isValidPosition(position, chain) {
+  const chainArr = chain.split("~~");
+  const posInArr = position - 1;
+
+  if (typeof position !== "number" || !isInt(position) || !chainArr[posInArr]) {
+    return false;
+  }
+  return true;
+}
+function isInt(n) {
+  return n % 1 === 0;
+}
 
 module.exports = chainMaker;
